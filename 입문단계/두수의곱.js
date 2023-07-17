0a55221e686f66509de7e3f189d2f7ec5c4ff88d
JAE HOON LEE <jazzyrain722@naver.com>
2023-07-17 22:53:18 +0900

    day1

--- 입문단계/두수의곱.js
+++ 입문단계/두수의곱.js
@@ -1,6 +1,6 @@
 function solution(num1, num2) {
   let answer;
-  if (num1 > 0 && num1 < 100 && num2 > 0 && num1 < 100) {
+  if (num1 >= 0 && num1 <= 100 && num2 >= 0 && num1 <= 100) {
     answer = num1 * num2;
   }
 
